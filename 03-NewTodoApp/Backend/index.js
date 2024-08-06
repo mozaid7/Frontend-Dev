283f3d21855e168fc2dee0092dd8c9f6b1de70fa
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-07 03:01:12 +0530

    requests created and connected to a DB

--- 03-NewTodoApp/Backend/index.js
+++ 03-NewTodoApp/Backend/index.js
@@ -1,52 +1,60 @@
-const express = require('express');
+const express = require("express");
 const { createTodo, updateTodo } = require("./types");
-const { todo } = require("./db")
+const { todo } = require("./db");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.post("/todo", async fuction(req, res) {
+app.post("/todo", async function(req, res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
+
     if (!parsedPayload.success) {
         res.status(411).json({
-            msg: "You sent the wrong input",
+            msg: "You sent the wrong inputs",
         })
         return;
     }
-    await todo.create ({
+    // put it in mongodb
+    await todo.create({
         title: createPayload.title,
         description: createPayload.description,
         completed: false
     })
+
     res.json({
         msg: "Todo created"
     })
 })
 
-app.get("/todos", fuction(req, res) {
-    const todos = todo.find({});
+app.get("/todos", async function(req, res) {
+    // const todos = await todo.find({});
 
     res.json({
-        todos
+        todos: []
     })
+
 })
 
-app.put("/completed", async fuction(req, res) {
+app.put("/completed", async function(req, res) {
     const updatePayload = req.body;
-    const parsedPayload = updateTodoTodo.safeParse(updatePayload);
+    const parsedPayload = updateTodo.safeParse(updatePayload);
     if (!parsedPayload.success) {
         res.status(411).json({
-            msg: "You sent the wrong input",
+            msg: "You sent the wrong inputs",
         })
         return;
     }
+
     await todo.update({
         _id: req.body.id
     }, {
-        completed: true
+      completed: true  
     })
-    res.json ({
+
+    res.json({
         msg: "Todo marked as completed"
     })
 })
