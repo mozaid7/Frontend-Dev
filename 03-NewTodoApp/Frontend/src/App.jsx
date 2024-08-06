9e5672c668fec08a23f7f3f8d61ce45979643adf
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-07 03:21:37 +0530

    createTodo tested

--- 03-NewTodoApp/Frontend/src/App.jsx
+++ 03-NewTodoApp/Frontend/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { CreateTodo } from './Components/CreateTodo'
 
 function App() {
-
   return (
     <div>
-      Hi There!
+     <CreateTodo></CreateTodo>
     </div>
   )
 }
