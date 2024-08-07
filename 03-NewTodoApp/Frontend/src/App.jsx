12c1f0bd4cfb9c7a190856d52e4a7347a2f5de07
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-07 14:00:22 +0530

    name letter changed

--- 03-NewTodoApp/Frontend/src/App.jsx
+++ 03-NewTodoApp/Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { CreateTodo } from './components/CreateTodo'
+import { CreateTodo } from './Components/CreateTodo'
 import { Todos } from './Components/Todos'
 
 // useEffect hook
