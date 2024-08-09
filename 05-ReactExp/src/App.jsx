33340ab02ea29758c073313dd1143e6c795e69c4
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-09 22:08:46 +0530

    re-rendering

--- 05-ReactExp/src/App.jsx
+++ 05-ReactExp/src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [title, setTitle] = useState("My name is Zaidy");
+
+  function updateTitle() {
+    setTitle("My name is " + Math.random())
+  }
+
   return (
     <div>
-      <Header title="Zaid" />
+      <button onClick={updateTitle}>Update the title</button>
+      <Header title={title} />
       <Header title="Zaid2" />
     </div>
   )
