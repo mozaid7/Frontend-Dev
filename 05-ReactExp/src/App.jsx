478f79ade1863daf08d302135b7a63abbf013cd1
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-10 16:38:31 +0530

    useEffect hook used to call the todos when 1st mounted

--- 05-ReactExp/src/App.jsx
+++ 05-ReactExp/src/App.jsx
@@ -3,14 +3,37 @@ import React, { Fragment } from 'react'
 import './App.css'
 
 function App() {
-  useEffect(function() {
-    alert("Hi")
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch("https://sum-server.100xdevs.com/todos")
+       .then(async function(res) {
+          const json = await res.json();
+          setTodos(json.todos);
+        })
+    }, 10000)
   }, [])
 
+  return (
+    <div>
+      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+    </div>
+  )
+}
+
+function Todo({title, description}) {
   return <div>
-    Hi There!
+    <h1>
+      {title}
+    </h1>
+    <h5>
+      {description}
+    </h5>
   </div>
 }
+
+
 // wrapping the components inside each other using wrapper
 // function App() {
 //   return <div>
