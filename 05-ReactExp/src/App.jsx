e5073348321563ef303f088d5be92764681f5404
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-11 16:59:47 +0530

    useEffect used to call todos from the server

--- 05-ReactExp/src/App.jsx
+++ 05-ReactExp/src/App.jsx
@@ -3,36 +3,36 @@ import React, { Fragment } from 'react'
 import './App.css'
 
 // useEffect hook used to call the todos when 1st mounted
-function App() {
-  const [todos, setTodos] = useState([])
+// function App() {
+//   const [todos, setTodos] = useState([])
 
-  useEffect(() => {
-    setInterval(() => {
-      fetch("https://sum-server.100xdevs.com/todos")
-       .then(async function(res) {
-          const json = await res.json();
-          setTodos(json.todos);
-        })
-    }, 10000)
-  }, [])
+//   useEffect(() => {
+//     setInterval(() => {
+//       fetch("https://sum-server.100xdevs.com/todos")
+//        .then(async function(res) {
+//           const json = await res.json();
+//           setTodos(json.todos);
+//         })
+//     }, 10000)
+//   }, [])
 
-  return (
-    <div>
-      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+//     </div>
+//   )
+// }
 
-function Todo({title, description}) {
-  return <div>
-    <h1>
-      {title}
-    </h1>
-    <h5>
-      {description}
-    </h5>
-  </div>
-}
+// function Todo({title, description}) {
+//   return <div>
+//     <h1>
+//       {title}
+//     </h1>
+//     <h5>
+//       {description}
+//     </h5>
+//   </div>
+// }
 
 
 // wrapping the components inside each other using wrapper
