39e1203e0853734e1c34420156344c23951a3f85
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-13 06:26:30 +0530

    boiler plate added

--- 07-Routing/src/App.jsx
+++ 07-Routing/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+     <Router>
+       <Route></Route>
+       <Route></Route>
+     </Router>
+    </BrowserRouter>
     
   )
 }
