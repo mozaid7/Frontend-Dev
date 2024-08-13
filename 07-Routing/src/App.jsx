25f426458d92612ab73b30eaf1c0b0ddfbd51e02
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-13 07:36:24 +0530

    comments added

--- 07-Routing/src/App.jsx
+++ 07-Routing/src/App.jsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+// lazy lets you defer loading component’s code until it is rendered for the first time.
 import {lazy, Suspense} from 'react';
 const Landing = lazy(() => import ('./Components/Landing'))
 const Dashboard = lazy(() => import ('./Components/Dashboard'))
 
+
+// <Suspense> lets you display a fallback until its children have finished loading.
 function App() {
   return (
   <div>
