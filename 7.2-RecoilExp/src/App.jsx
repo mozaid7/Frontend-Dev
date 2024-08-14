acdbc8f31b13a51f2dedd334f4d7f31959e3a463
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-14 15:33:30 +0530

    buttons and logic to get the notifi. no. created

--- 7.2-RecoilExp/src/App.jsx
+++ 7.2-RecoilExp/src/App.jsx
@@ -1,15 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom, totalNotificationSelector } from './atoms'
+import { useMemo } from 'react'
+//RecoilRoot
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <RecoilRoot>
+    <MainApp />
+  </RecoilRoot>
+}
+
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(networkAtom)
+  const jobsAtomCount = useRecoilValue(jobsAtom);
+  const notificationsAtomCount = useRecoilValue(notificationsAtom)
+  const messagingAtomCount = useRecoilValue(messagingAtom)
+  const totalNotificationCount = useRecoilValue(totalNotificationSelector);
+
+  // const totalNotificationCount = useMemo(() => {
+  //   return networkNotificationCount + jobsAtomCount + notificationsAtomCount + messagingAtomCount;
+  // }, [networkNotificationCount, jobsAtomCount, notificationsAtomCount, messagingAtomCount]) 
 
   return (
-    <div>
+    <>
+      <button>Home</button>
       
-    </div>
+      <button>My network ({networkNotificationCount >= 100 ? "99+" : networkNotificationCount})</button>
+      <button>Jobs {jobsAtomCount}</button>
+      <button>Messaging ({messagingAtomCount})</button>
+      <button>Notifications ({notificationsAtomCount})</button>
+
+      <button>Me ({totalNotificationCount})</button>
+    </>
   )
 }
 
