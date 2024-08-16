08e287f11a561c10ab95813a2770630d394f8246
Mohammed Zaid <mohammedzee98@gmail.com>
2024-08-16 09:43:44 +0530

    atomFamily created to get particulR TODO from the todo file

---
+++ 7.1-Recoil/Atom-family/src/atoms.js
@@ -0,0 +1,23 @@
+import { atomFamily } from "recoil";
+import { TODOS } from "./todos";
+
+export const todosAtomFamily = atomFamily({
+  key: 'todosAtomFamily',
+  default: id => {
+    return TODOS.find(x => x.id === id)
+  },
+});
+
+// OR 
+// export const todosAtomFamily = atomFamily({
+//     key: 'todosAtomFamily',
+//     default: id => {
+//         let foundTodo = null;
+//         for (let i = 0; i < TODOS.length; i++) {
+//             if (TODOS.[i].id === id) {
+//                 foundTodo = TODOS[i]
+//             }
+//         }
+//         return foundTodo
+//     },
+// });
